e1e0fb81d3ea48a26e118d10ca541d4356548516
gryun0330@gm.gist.ac.kr <gryun0330@gm.gist.ac.kr>
2026-04-11 13:52:44 +0900

    asdf

--- devices/timer.c
+++ devices/timer.c
@@ -176,17 +176,6 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
-  if(thread_mlfqs) {
-    mlfqs_increment_recent_cpu();
-    if(ticks%4 == 0) {
-        mlfqs_recalculate_priority();
-        if(ticks%TIMER_FREQ==0){
-            mlfqs_recalculate_recent_cpu();
-            mlfqs_update_load_avg();
-
-      }
-}
-  }
   thread_awake(ticks);	// do the job at every ticks
 }
 
